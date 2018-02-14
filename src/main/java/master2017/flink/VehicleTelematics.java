f117419bf9726d96e9ccb34ccf38742feb563096
Massimiliano R <mrusso.pro@gmail.com>
2018-02-14 09:54:33 +0100

    Minor changes

--- src/main/java/master2017/flink/VehicleTelematics.java
+++ src/main/java/master2017/flink/VehicleTelematics.java
@@ -10,18 +10,18 @@ import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.api.functions.timestamps.AscendingTimestampExtractor;
 import org.apache.flink.streaming.api.functions.windowing.WindowFunction;
 import org.apache.flink.streaming.api.windowing.assigners.EventTimeSessionWindows;
-import org.apache.flink.streaming.api.windowing.assigners.SlidingEventTimeWindows;
 import org.apache.flink.streaming.api.windowing.time.Time;
 import org.apache.flink.streaming.api.windowing.windows.TimeWindow;
 import org.apache.flink.util.Collector;
 
-import java.util.ArrayList;
-import java.util.Iterator;
+import java.util.*;
 
 public class VehicleTelematics {
 
     private static DataStream<Tuple8<Integer, Integer, Integer, Integer, Integer, Integer, Integer, Integer>> parsedStream;
     private static SingleOutputStreamOperator<Tuple8<Integer, Integer, Integer, Integer, Integer, Integer, Integer, Integer>> parsedTimedStream;
+
+    private static Set<Integer> ALL_SEGMENTS = new HashSet<Integer>(Arrays.asList(52, 53, 54, 55, 56));
 
     public static void main(String[] args) throws Exception {
 
@@ -72,35 +72,41 @@ public class VehicleTelematics {
 
         parsedTimedStream
                 .keyBy(0)                                                                                // Using the timestamp as key to split the dataset
-                .filter( tuple -> tuple.f2 >= 90)                                                               // only those with speed >= 90mph
+                .filter( tuple -> tuple.f2 >= 90)                                                               // only those with speedSum >= 90mph
                 .map( tuple -> new Tuple6<>(tuple.f0, tuple.f1, tuple.f3, tuple.f6, tuple.f5, tuple.f2))        // re-arrange the fields
                 .writeAsCsv(OUTPUT_FOLDER_PATH + "/" + "speedfines.csv", FileSystem.WriteMode.OVERWRITE)   // Write the output into a new file
                 .setParallelism(1);                                                                             // setPar to 1 to create only ONE file
-
+*/
 
         //
         // 2nd ALERT avgSpeedAlert
         //
 
         parsedTimedStream
-                .filter(event -> event.f6 >= 52 && event.f6 <= 56)                        // get only those in between 52 and 56
-                .keyBy(1, 3, 5)                                                    // key by id, xway and direction
-                .window(EventTimeSessionWindows.withGap(Time.seconds(31)))                // get windows
-                .apply(new ComputeWindowEvent())                                          // get the tuple if vehicle went by all segments and avg speed >= 60mph
-                .writeAsCsv(OUTPUT_FOLDER_PATH + "/" + "avgspeedfines.csv", FileSystem.WriteMode.OVERWRITE)
-                .setParallelism(1);                                                       // setPar to 1 to create only ONE file
-*/
+                .filter(event -> event.f6 >= 52 && event.f6 <= 56)                  // get only those in between 52 and 56
+                .keyBy(1, 3, 5)                                              // key by id, xway and direction
+                .window(EventTimeSessionWindows.withGap(Time.seconds(31)))          // get event windows that close as soon as a car doesn't give information for more than 30 secs
+                .apply(new ComputeWindowEvent())                                    // get the tuple if vehicle went by all segments and avg speedSum >= 60mph
+                .writeAsCsv(OUTPUT_FOLDER_PATH + "/" + "avgspeedfines.csv",
+                            FileSystem.WriteMode.OVERWRITE)
+                .setParallelism(1);                                                 // setPar to 1 to create only ONE file
+
+/*
         //
         // 3rd ALERT
         //
 
         parsedTimedStream
-                .filter( tuple -> tuple.f2 == 0)                                            // only vehicles with null speed can crash
-                .keyBy(1, 3, 5)                                                      // key by id, highway and direction
-                .window(SlidingEventTimeWindows.of(Time.seconds(30 * 4), Time.seconds(30))) // get windows of 2min every 30secs
+                .filter( tuple -> tuple.f2 == 0)                                // only vehicles with null speedSum can crash
+                .keyBy(1, 3, 5)                                                 // key by id, highway and direction
+                .window(SlidingEventTimeWindows.of(Time.seconds(30 * 4),
+                                                   Time.seconds(30)))           // get windows of 2min every 30secs
                 .apply(new CheckForCollisions())
-                .writeAsCsv(OUTPUT_FOLDER_PATH + "/" + "accidents.csv", FileSystem.WriteMode.OVERWRITE)
+                .writeAsCsv(OUTPUT_FOLDER_PATH + "/" + "accidents.csv",
+                            FileSystem.WriteMode.OVERWRITE)
                 .setParallelism(1);
+*/
+
 
         env.execute();
 
@@ -118,45 +124,49 @@ public class VehicleTelematics {
             implements WindowFunction<Tuple8<Integer, Integer, Integer, Integer, Integer, Integer, Integer, Integer>,
             Tuple6<Integer, Integer, Integer, Integer, Integer, Integer>, Tuple, TimeWindow> {
 
+        Iterator<Tuple8<Integer, Integer, Integer, Integer, Integer, Integer, Integer, Integer>> window;
+        Tuple8<Integer, Integer, Integer, Integer, Integer, Integer, Integer, Integer> event;
+        int eventCount, speedSum, minTime, maxTime;
+        boolean oneway;
+        Set<Integer> segmentSet;
+
         @Override
         public void apply(Tuple tuple, TimeWindow timeWindow,
                 Iterable<Tuple8<Integer, Integer, Integer, Integer, Integer, Integer, Integer, Integer>> iterable,
-                    Collector<Tuple6<Integer, Integer, Integer, Integer, Integer, Integer>> collector) throws Exception {
+                    Collector<Tuple6<Integer, Integer, Integer, Integer, Integer, Integer>> collector) {
 
+            eventCount = 0;
+            speedSum = 0;
+            minTime = Integer.MAX_VALUE;
+            maxTime = 0;
+            oneway = true;
+            segmentSet = new HashSet<>();
 
-            Iterator<Tuple8<Integer, Integer, Integer, Integer, Integer, Integer, Integer, Integer>> window = iterable.iterator();
-            Tuple8<Integer, Integer, Integer, Integer, Integer, Integer, Integer, Integer> event = null;
+            window = iterable.iterator();
 
-            int count = 0;
-            int speed = 0;
-            int minTime = Integer.MAX_VALUE;
-            int maxTime = 0;
-            boolean oneway = true;
-            ArrayList<Integer> segmentSet = new ArrayList<Integer>();
+            while (window.hasNext() /*&& oneway*/) {
 
-            while (window.hasNext() && oneway) {
                 event = window.next();
+                eventCount ++;
 
                 minTime = (event.f0 <= minTime) ? event.f0 : minTime;
                 maxTime = (event.f0 >= maxTime) ? event.f0 : maxTime;
 
-                speed += event.f2;
-                count += 1;
+                speedSum += event.f2;
                 segmentSet.add(event.f6);
-                oneway = (segmentSet.get(0) == 52 && segmentSet.get(segmentSet.size()-1) <= event.f6
-                        || segmentSet.get(0) == 56 && segmentSet.get(segmentSet.size()-1) >= event.f6) ? true : false;
+                //oneway = segmentSet.get(0) == 52 && segmentSet.get(segmentSet.size() - 1) <= event.f6
+                //        || segmentSet.get(0) == 56 && segmentSet.get(segmentSet.size() - 1) >= event.f6;
                 // One way means from 52 to 56 without return
 
             }
 
-            if(oneway
-                    && segmentSet.contains(52)
-                    && segmentSet.contains(53)
-                    && segmentSet.contains(54)
-                    && segmentSet.contains(55)
-                    && segmentSet.contains(56)
-                    && speed/count >= 60) //Speed
-                collector.collect(new Tuple6<>(minTime, maxTime, event.f1, event.f3, event.f5, speed/count));
+            if(!segmentSet.containsAll(ALL_SEGMENTS))
+                return;
+
+            if(speedSum / eventCount < 60)
+                return;
+
+            collector.collect(new Tuple6<>(minTime, maxTime, event.f1, event.f3, event.f5, speedSum / eventCount));
         }
     }
 
@@ -182,10 +192,10 @@ public class VehicleTelematics {
             Tuple8<Integer, Integer, Integer, Integer, Integer, Integer, Integer, Integer> event
                     = window.next();
 
-            // If the event is null or the speed is not 0
+            // If the event is null or the speedSum is not 0
             //  then we cannot raise an alert for collision in the current window
-            if (event == null)
-                return;
+            //if (event == null)
+            //    return;
 
             // If the event is valid, initialize some variables
             initTime = event.f0;
@@ -199,11 +209,11 @@ public class VehicleTelematics {
 
                 event = window.next();
 
-                // If the event is null or the car moved we cannot report an alert
-                if(event == null || event.f7 != initPos)
+                // If the car moved we cannot report an alert
+                if(event.f7 != initPos)
                     return;
 
-                // Otherwise we count it
+                // Otherwise we count it as a valid event
                 numberOfEvents++;
             }
 
